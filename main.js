9193d5de5d0d60db6d6a29a0565655cd0814f3c3
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-06-08 00:53:43 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -83,10 +83,9 @@ document.querySelector("#cards").innerHTML =
   ) +
 
   createCard('12/06', 'sexta',
-    createGame("Canadá", "x", "Bósnia e Hern") +
+    createGame("Canadá", "x", "Bósnia e Herz") +
     createGame("Catar", "x", "Suiça") +
     createGame("EUA", "x", "Paraguai") +
-    createGame("Austrália", "x", "Turquia")
   ) +
 
   createCard('13/06', 'sábado',
@@ -95,17 +94,18 @@ document.querySelector("#cards").innerHTML =
   ) +
 
   createCard('14/06', 'domingo',
+    createGame("Austrália", "x", "Turquia") +
     createGame("Alemanha", "x", "Curaçau") +
     createGame("Costa do Marfim", "x", "Equador") +
     createGame("Holanda", "x", "Japão") +
-    createGame("Bósnia e Herz.", "x", "Tunisia")  
+    createGame("Suécia", "x", "Tunisia")  
   ) +
 
   createCard('15/06', 'segunda',
-    createGame("Bélgica", "x", "Egito") +
-    createGame("Irã", "x", "Nova Zelândia") +
     createGame("Espanha", "x", "Cabo Verde") +
-    createGame("Arabia Saudita", "x", "Uruguai")
+    createGame("Bélgica", "x", "Egito") +
+    createGame("Arabia Saudita", "x", "Uruguai") +
+    createGame("Irã", "x", "Nova Zelândia")
   ) +
 
   createCard('16/06', 'terça',
@@ -117,99 +117,98 @@ document.querySelector("#cards").innerHTML =
 
   createCard('17/06', 'quarta',
     createGame("Portugal", "x", "RD_Congo") +
-    createGame("Uzbequistão", "x", "Colômbia") +
     createGame("Inglaterra", "x", "Croácia") +
     createGame("Gana", "x", "Panamá") + 
-    createGame("México", "x", "Coreia do Sul") +
-    createGame("Àfrica do Sul", "x", "República Tcheca")
+    createGame("Uzbequistão", "x", "Colômbia")  
   ) +
 
   createCard('18/06', 'quinta',
-    createGame("Canadá", "x", "Catar") +
-    createGame("Bósnia e Hern", "x", "Suiça")
+    createGame("República Tcheca", "x", "África do Sul") +
+    createGame("Suiça", "x", "Bósnia e Herz") +
+    createGame("Canadá", "x", "Catar") + 
+    createGame("México", "x", "Coreia do Sul") + 
   ) +
 
   createCard('19/06', 'sexta',
+    createGame("EUA", "x", "Austrália") +
     createGame("Escócia", "x", "Marrocos") +
-    createGame("Brasil", "x", "Haiti") +    
-    createGame("República Tcheca", "x", "Paraguai") +
-    createGame("EUA", "x", "Austrália") 
+    createGame("Brasil", "x", "Haiti") +     
   ) +
 
   createCard('20/06', 'sabado',
+    createGame("Holanda", "x", "Suécia") +
     createGame("Alemanha", "x", "Costa do Marfim") +
     createGame("Equador", "x", "Curaçau") 
   ) +
 
   createCard('21/06', 'domingo',
-    createGame("Holanda", "x", "Bósnia e Herz.") +
     createGame("Tunisia", "x", "Japão") +
+    createGame("Espanha", "x", "Arabia Saudita") +
     createGame("Bélgica", "x", "Irã") +
+    createGame("Uruguai", "x", "Cabo Verde") +
     createGame("Nova Zelândia", "x", "Egito") 
   ) +
 
-  createCard('22/06', 'segunda',
-    createGame("Espanha", "x", "Arabia Saudita") +
-    createGame("Uruguai", "x", "Cabo Verde") +  
+  createCard('22/06', 'segunda',  
+    createGame("Argentina", "x", "Àustria") +
     createGame("França", "x", "Iraque") +    
     createGame("Noruega", "x", "Senegal") 
   ) +
 
   createCard('23/06', 'terça',
-    createGame("Argentina", "x", "Àustria") +
     createGame("Jordânia", "x", "Argélia") +
     createGame("Portugal", "x", "Uzbequistão") +
-    createGame("Colômbia", "x", "RD_Congo")+
     createGame("Inglaterra", "x", "Gana") +
-    createGame("Croácia", "x", "Panamá")
+    createGame("Panamá", "x", "Croácia") + 
+    createGame("Colômbia", "x", "RD_Congo") 
   ) +
 
   createCard('24/06', 'quarta',
-    createGame("México", "x", "Suécia") +
-    createGame("Àfrica do Sul", "x", "Coreia do Sul") +
     createGame("Suiça", "x", "Canadá") +
-    createGame("Turquia", "x", "Catar") +
+    createGame("Bosnia e Herz", "x", "Catar") +
     createGame("Escócia", "x", "Brasil") +
-    createGame("Marrocos", "x", "Haiti")
+    createGame("Marrocos", "x", "Haiti") +
+    createGame("México", "x", "Suécia") +
+    createGame("Àfrica do Sul", "x", "Coreia do Sul") 
   ) +
 
   createCard('25/06', 'quinta',
-    createGame("República Tcheca", "x", "EUA") +
-    createGame("Paraguai", "x", "Austrália") +
     createGame("Equador", "x", "Alemanha") +
-    createGame("Curaçau", "x", "Costa do Marfim")
+    createGame("Curaçau", "x", "Costa do Marfim") +
+    createGame("Japão", "x",   "Suécia") +
+    createGame("Tunisia", "x", "Holanda") +
+    createGame("Turquia", "x", "EUA") +
+    createGame("Paraguai", "x", "Austrália") +
   ) +
 
   createCard('26/06', 'sexta',
-    createGame("Tunisia", "x", "Holanda") +
-    createGame("Japão", "x",   "Bósnia e Herz.") +
-    createGame("Nova Zelândia", "x", "Bélgica") +
-    createGame("Egito", "x", "Irã") +
+    createGame("Senegal", "x", "Iraque") +
+    createGame("Noruega", "x", "França") +
     createGame("Uruguai", "x", "Espanha") +
-    createGame("Cabo Verde", "x", "Arabia Saudita")
-
+    createGame("Cabo Verde", "x", "Arabia Saudita") 
   ) +
 
   createCard('27/06', 'sabado',
-    createGame("Noruega", "x", "França") +
-    createGame("Senegal", "x", "Iraque") +
-    createGame("Jordânia", "x", "Argentina") +
-    createGame("Argélia", "x", "Àustria") +     
+    createGame("Egito", "x", "Irã") +
+    createGame("Nova Zelandia", "x", "Bélgica") +
+    createGame("Panamá", "x", "Inglaterra") +
+    createGame("Croácia", "x", "Gana") +
     createGame("Colômbia", "x", "Portugal") +
     createGame("RD_Congo", "x", "Uzbequistão") +
-    createGame("Panamá", "x", "Inglaterra") +
-    createGame("Croácia", "x", "Gana")
+    createGame("Jordânia", "x", "Argentina") +
+    createGame("Argélia", "x", "Àustria")     
   );
 
   //Fase de Mata Mata 
 
 //   createCard('28/06', 'domingo',
-//     createGame("", "", "") +
-//     createGame("", "", "")
+//     createGame("2A", "x", "2B") 
 //   ) +
 
 //   createCard('29/06', 'segunda',
-//     createGame("", "", "
+//     createGame("1C", "x", "2F") +
+       //createGame("1E", "x", "3ABCDF") +
+      // createGame("1F", "x", "2C") 
 //   ) +
 
 //   createCard('/', '',
