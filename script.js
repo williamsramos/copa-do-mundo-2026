119a27c41e82002485b89ccf396158f0999214a9
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-06-12 11:47:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -199,16 +199,16 @@ let jogosDetalhados = JSON.parse(localStorage.getItem("jogosSimulador")) || [
   { casa:"Croácia", fora:"Gana", estadio:"Filadélfia", hora:"18:00", placarCasa:"", placarFora:"" }
 ]},
 
-// ================= MATA-MATA - DATAS A DEFINIR =================
+// ================= MATA-MATA - AJUSTE NAS FINAIS =================
 { grupo:"Mata-mata", rodada:"16 avos de Final", data:"28/06 a 03/07", jogos:[
-  { id: 73, casa:"2º Grupo A", fora:"2º Grupo B", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 74, casa:"1º Grupo E", fora:"3º A/B/C/D/F", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 75, casa:"1º Grupo F", fora:"2º Grupo C", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 76, casa:"1º Grupo C", fora:"2º Grupo F", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 77, casa:"1º Grupo I", fora:"3º C/D/F/G/H", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 78, casa:"2º Grupo E", fora:"2º Grupo I", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 79, casa:"1º Grupo A", fora:"3º C/E/F/H/I", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 80, casa:"1º Grupo L", fora:"3º E/H/I/J/K", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 73, casa:"2º Grupo A", fora:"2º Grupo B", estadio:"Inglewood", data:"28/06", hora:"16:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 74, casa:"1º Grupo E", fora:"3º A/B/C/D/F", estadio:"Foxborough", data:"29/06", hora:"17:30", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 75, casa:"1º Grupo F", fora:"2º Grupo C", estadio:"Monterrey", data:"29/06", hora:"22:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 76, casa:"1º Grupo C", fora:"2º Grupo F", estadio:"Houston", data:"29/06", hora:"14:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 77, casa:"1º Grupo I", fora:"3º C/D/F/G/H", estadio:"East Rutherfood", data:"30/06", hora:"18:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 78, casa:"2º Grupo E", fora:"2º Grupo I", estadio:"Arlington", data:"30/06", hora:"14:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 79, casa:"1º Grupo A", fora:"3º C/E/F/H/I", estadio:"Cidade do México", data:"30/06", hora:"22:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 80, casa:"1º Grupo L", fora:"3º E/H/I/J/K", estadio:"Atlanta", data:"01/07", hora:"13:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
   { id: 81, casa:"1º Grupo D", fora:"3º B/E/F/I/J", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
   { id: 82, casa:"1º Grupo G", fora:"3º A/E/H/I/J", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
   { id: 83, casa:"2º Grupo K", fora:"2º Grupo L", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
@@ -239,8 +239,9 @@ let jogosDetalhados = JSON.parse(localStorage.getItem("jogosSimulador")) || [
   { id: 102, casa:"Vencedor 99 (QF V3)", fora:"Vencedor 100 (QF V4)", estadio:"A definir", data:"A definir", hora:"--:--", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" }
 ]},
 { grupo:"Mata-mata", rodada:"Finais", data:"18/07 e 19/07", jogos:[
-  { id: 103, casa:"Perdedor 101", fora:"Perdedor 102", estadio:"Miami Gardens, Flórida", data:"18/07" , hora:"18:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
-  { id: 104, casa:"Vencedor 101", fora:"Vencedor 102", estadio:"East Rutherford, New Jersey", data:"19/07" , hora:"16:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" }
+  // Atualizado com a data de cada partida individualmente:
+  { id: 103, casa:"Perdedor 101", fora:"Perdedor 102", estadio:"Miami Gardens, Flórida", data:"18/07", hora:"18:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 104, casa:"Vencedor 101", fora:"Vencedor 102", East Rutherford, New Jersey", data:"19/07", hora:"16:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" }
 ]}
 ];
 
