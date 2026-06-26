970cbdddf1b13c5838f9cfb5282f79bd7e16a9f1
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-06-26 00:03:07 -0300

    Atualizar o classificacao.js

--- classificacao.js
+++ classificacao.js
@@ -37,29 +37,45 @@ function renderTabela(){
           <tr><th>Pos</th><th>Time</th><th>P</th><th>J</th><th>V</th><th>E</th><th>D</th><th>GP</th><th>GC</th><th>SG</th></tr>
     `;
 
-    times.forEach(([nome,d],i)=>{
-      const jogosTotal = d.v + d.e + d.d;
-      let classeCSS = "";
-      if(terceiraRodadaCompleta){
-        if(i === 0 || i === 1) classeCSS = "qualificado";
-        else if(i === 2) classeCSS = "terceiro";
-      }
+ // Dentro da sua função renderTabela() no classificacao.js:
+times.forEach(([nome, d], i) => {
+  const posicao = i + 1;
+  let statusTexto = "";
+  let statusClasse = "";
 
-      html += `
-        <tr class="${classeCSS}">
-          <td>${i + 1}</td>
-          <td>${getBandeira(nome)} ${nome}</td>
-          <td>${d.pts}</td>
-          <td>${jogosTotal}</td>
-          <td>${d.v}</td>
-          <td>${d.e}</td>
-          <td>${d.d}</td>
-          <td>${d.gp}</td>
-          <td>${d.gc}</td>
-          <td>${d.gp - d.gc}</td>
-        </tr>
-      `;
-    });
+  // Define o texto e a classe CSS com base na posição exata
+  if (posicao <= 2) {
+    statusTexto = "16 avos de final";
+    statusClasse = "status-classificado";   // Verde para 1º e 2º
+  } else if (posicao === 3) {
+    statusTexto = "16 avos de final";
+    statusClasse = "status-terceiro-azul";  // Azul claro para o 3º
+  } else if (posicao === 4) {
+    statusTexto = "Eliminado";
+    statusClasse = "status-eliminado";      // Vermelho para o 4º
+  }
+
+  const jogosTotal = d.v + d.e + d.d;
+
+  html += `
+    <tr class="${statusClasse}">
+      <td>${posicao}</td>
+      <td style="text-align:left; padding-left:10px;">${getBandeira(nome)} ${nome}</td>
+      <td>${d.pts}</td>
+      <td>${jogosTotal}</td>
+      <td>${d.v}</td>
+      <td>${d.e}</td>
+      <td>${d.d}</td>
+      <td>${d.gp}</td>
+      <td>${d.gc}</td>
+      <td>${d.gp - d.gc}</td>
+      <td style="font-size: 11px; font-weight: bold; text-transform: uppercase;">
+        ${statusTexto}
+      </td>
+    </tr>
+  `;
+});
+
     html += `</table></div>`;
     div.innerHTML += html;
   }
