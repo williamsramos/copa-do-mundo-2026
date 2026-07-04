09b7b00eb76fc1cc7d97a4700bce4ca025f7180b
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-03 21:58:39 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -236,19 +236,19 @@ createCard('04/07 a 07/07', 'OITAVAS DE FINAL',
   createGame("Brasil", "x", "Noruega") +
   createGame("México", "x", "Inglaterra") +
   createGame("EUA", "x", "Bélgica") 
-  // createGame("Portugal", "x", "Espanha") +
-  // createGame("Vencedor 86(ARGxCBV)", "x", "Egito") +
+  createGame("Portugal", "x", "Espanha") +
+  createGame("Argentina", "x", "Egito") +
   // createGame("Suiça", "x", "Vencedor 87(ColômbiaxGana)")
-)  /*+
+)  /* +
 
 // 👑 DIVISOR: QUARTAS DE FINAL
 `<div class="fase-divisor" style="width: 100%; text-align: center; margin: 40px 0 20px; color: #00dc4d; font-family: sans-serif; font-size: 1.5rem; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">💪 Quartas de Final 💪</div>` +
 
 createCard('09/07 a 11/07', 'QUARTAS DE FINAL',
-  createGame("Vencedor 90", "x", "Vencedor 89") +
-  createGame("Vencedor 91", "x", "Vencedor 92") +
-  createGame("Vencedor 93", "x", "Vencedor 94") +
-  createGame("Vencedor 95", "x", "Vencedor 96")
+  createGame("Vencedor 90(CanxMar)", "x", "Vencedor 89(ParxFra)") +
+  createGame("Vencedor 91(BraxNor)", "x", "Vencedor 92(MéxXIng)") +
+  createGame("Vencedor 93(EuaxBel)", "x", "Vencedor 94(PorxEsp)") +
+  createGame("Vencedor 95(ArgxEgi)", "x", "Vencedor 96(SuixColxGan)")
 ) +
 
 // 👑 DIVISOR: SEMIFINAIS
