8733670e806b4120f4ad287663485842cad2e7c4
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-04 16:10:05 -0300

    Atualizar o dados.js

--- dados.js
+++ dados.js
@@ -221,7 +221,7 @@ const jogosPadrao = [
   { "id": 87, "casa": "1º Grupo K", "fora": "3º Melhor 3", "estadio": "Kansas City", "data": "03/07", "hora": "22:30", "placarCasa": "1", "placarFora": "0", "penaisCasa": "", "penaisFora": "" }
 ]},
 { grupo:"Mata-mata", rodada:"Oitavas de Final", data:"04/07 a 07/07", jogos:[
-  { id: 90, casa:"Jogo 73", fora:"Jogo 75", estadio:"Houston", data:"04/07", hora:"14:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
+  { id: 90, casa:"Jogo 73", fora:"Jogo 75", estadio:"Houston", data:"04/07", hora:"14:00", placarCasa:"0", placarFora:"3", penaisCasa:"", penaisFora:"" },
   { id: 89, casa:"Jogo 74", fora:"Jogo 77", estadio:"Filadélfia", data:"04/07", hora:"18:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
   { id: 91, casa:"Jogo 76", fora:"Jogo 78", estadio:"New York/New Jersey", data:"05/07", hora:"17:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
   { id: 92, casa:"Jogo 79", fora:"Jogo 80", estadio:"Cidade do México", data:"05/07", hora:"21:00", placarCasa:"", placarFora:"", penaisCasa:"", penaisFora:"" },
