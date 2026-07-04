e317fcc990d3172cb2b82652550b9208a0b8e75f
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-03 22:01:35 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -235,7 +235,7 @@ createCard('04/07 a 07/07', 'OITAVAS DE FINAL',
   createGame("Paraguai", "x", "França") +
   createGame("Brasil", "x", "Noruega") +
   createGame("México", "x", "Inglaterra") +
-  createGame("EUA", "x", "Bélgica") 
+  createGame("EUA", "x", "Bélgica") +
   createGame("Portugal", "x", "Espanha") +
   createGame("Argentina", "x", "Egito") 
   // createGame("Suiça", "x", "Vencedor 87(ColômbiaxGana)")
