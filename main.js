b775004fa65d639932151e165c66f3bdee871bda
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-12 00:48:17 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -254,18 +254,18 @@ document.querySelector("#cards").innerHTML =
 
 createCard('14/07 a 15/07', 'SEMIFINAL',
   createGame("França", "x", "Espanha") +
-  createGame("Inglaterra", "x", "Vencedor 100(ArgXSui)")
+  createGame("Inglaterra", "x", "Argentina")
 ) +
 
 // 👑 DIVISOR: DECISÕES FINAIS
 `<div class="fase-divisor" style="width: 100%; text-align: center; margin: 40px 0 20px; color: #f7dd43; font-family: sans-serif; font-size: 1.5rem; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">✨ Finais ✨</div>` +
 
 createCard('18/07', 'Disputa do 3º Lugar',
-  createGame("Perdedor 101(FraXESp)", "x", "Perdedor 102(ArgxSuiXIng)") 
+  createGame("Perdedor 101(FraXESp)", "x", "Perdedor 102(ArgxIng)") 
 ) +
 
 createCard('19/07', 'Grande Final',
-createGame("Vencedor 101(FraXESp)", "x", "Vencedor 102(ArgxSuiXIng)")
+createGame("Vencedor 101(FraXESp)", "x", "Vencedor 102(ArgxIng)")
 ) +
 
 // 🥇 BLOCO DO PÓDIO: Agora limpo, usando as classes do CSS!
