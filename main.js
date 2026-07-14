9ee2adf9ad878cea25b3800b5f4f7a4d15347bdb
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-14 18:21:17 -0300

    Update game matchups for semifinals and finals

--- main.js
+++ main.js
@@ -253,7 +253,7 @@ document.querySelector("#cards").innerHTML =
 `<div class="fase-divisor" style="width: 100%; text-align: center; margin: 40px 0 20px; color: #fd0000; font-family: sans-serif; font-size: 1.5rem; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">🏁 Semifinais 🏁</div>` +
 
 createCard('14/07 a 15/07', 'SEMIFINAL',
-  createGame("França", "x", "Espanha") +
+  //createGame("França", "x", "Espanha") +
   createGame("Inglaterra", "x", "Argentina")
 ) +
 
@@ -261,11 +261,11 @@ createCard('14/07 a 15/07', 'SEMIFINAL',
 `<div class="fase-divisor" style="width: 100%; text-align: center; margin: 40px 0 20px; color: #f7dd43; font-family: sans-serif; font-size: 1.5rem; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">✨ Finais ✨</div>` +
 
 createCard('18/07', 'Disputa do 3º Lugar',
-  createGame("Perdedor 101(FraXESp)", "x", "Perdedor 102(ArgxIng)") 
+  createGame("França", "x", "Perdedor 102(ArgxIng)") 
 ) +
 
 createCard('19/07', 'Grande Final',
-createGame("Vencedor 101(FraXESp)", "x", "Vencedor 102(ArgxIng)")
+createGame("Espanha", "x", "Vencedor 102(ArgxIng)")
 ) +
 
 // 🥇 BLOCO DO PÓDIO: Agora limpo, usando as classes do CSS!
