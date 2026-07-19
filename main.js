cbf2f274956cadb32097fa78bb1016663236a1f2
William Souza Ramos <66261416+williamsramos@users.noreply.github.com>
2026-07-18 22:42:32 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -261,11 +261,11 @@ document.querySelector("#cards").innerHTML =
 `<div class="fase-divisor" style="width: 100%; text-align: center; margin: 40px 0 20px; color: #f7dd43; font-family: sans-serif; font-size: 1.5rem; font-weight: bold; text-transform: uppercase; letter-spacing: 2px;">✨ Finais ✨</div>` +
 
 createCard('18/07', 'Disputa do 3º Lugar', '18 HORAS',
-  createGame("França", "x", "Inglaterra") 
+  createGame("França 4", "x", "6 Inglaterra") 
 ) +
 
 createCard('19/07', 'Grande Final',  '16 HORAS',
-createGame("Espanha", "x", "Argentina")
+   createGame("Espanha", "x", "Argentina")
 ) +
 
 // 🥇 BLOCO DO PÓDIO: Agora limpo, usando as classes do CSS!
